1736e1b6c3c10c91e078cdb431a1fe9ef34e4fbe
Антонина Арабаджи <tonyaarabadzi@mail.ru>
2025-02-22 10:34:29 +0300

    Добавьте файлы проекта.

---
+++ MathOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ввидите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            Console.WriteLine($"сумма: {sum}");
+            Console.ReadKey();
+
+        }
+    }
+}
